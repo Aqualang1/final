7588e66e92bdc51441eb83043db2bdd5b055d71a
Ivan <ivanshvetcov@gmail.com>
2023-08-10 01:56:57 +0300

    Products preview page update

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -1,7 +1,7 @@
 import './Card.css';
 import Laptop from '../../assets/laptop.png'
 import ItemDescription from '../../components/ItemDescription/ItemDescription';
-import ItemProperties from '../../components/ItemProperties';
+import ItemProperties from '../../components/ItemProperties/ItemProperties';
 import Status from '../../components/Status/Status';
 
 const Card = () => {
